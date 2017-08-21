4567829bd0e74c3d94fb24247f0d780bc2b56523
svetlana <you@example.com>
2017-08-21 09:42:18 +0300

    3 commit

--- Calculate.java
+++ Calculate.java
@@ -1,6 +1,10 @@
 public class Calculate{
     public static  void main(String[] arg){
-        System.out.print("YRAAAAAAAAA");
+        System.out.println("YRAAAAAAAAA");
+        int first = Integer.valueOf(arg [0]);
+        int second = Integer.valueOf(arg[1]);
+        int sum = first + second;
+        System.out.println("Summa " + sum);
       }
 
 }
