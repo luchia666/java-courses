cabe3e0e99baffca7082227e31d5e15345770242
svetlana <you@example.com>
2017-08-22 15:29:07 +0300

    number 3

--- Calculator.java
+++ Calculator.java
@@ -1,17 +1,27 @@
 public class Calculator{
     
-    private int result;
+    private float result;
     
-    
-    public Calculator(){}
-    
-    public void add (int ... params){
-      for (Integer param : params){
-          this.result += param;
+     
+    public void add (float ... params){
+      for (float param : params){
+        this.result += param;
       }
     }
     
-    public int getResult(){
+    public void minus (float param1, float param2){
+        this.result = param1 - param2;
+    }
+    
+    public void multiplication (float param1, float param2){
+        this.result = param1 * param2;
+     }     
+     
+    public void division (float param1, float param2){
+        this.result =  param1/param2;
+     } 
+    
+    public float getResult(){
         return this.result;    
     }
     
@@ -19,12 +29,12 @@ public class Calculator{
     this.result = 0;
     }
     
-//    public static  void main(String[] arg){
+    public static  void main(String[] arg){
 //        System.out.println("YRAAAAAAAAA");
 //        int first = Integer.valueOf(arg [0]);
 //        int second = Integer.valueOf(arg[1]);
 //        int sum = first + second;
 //        System.out.println("Summa " + sum);
-//      }
+      }
 
 }
--- InteractRunner.java
+++ InteractRunner.java
@@ -4,19 +4,42 @@ public class InteractRunner{
     public static  void main(String[] arg){
         Scanner reader = new Scanner(System.in);
         try {
-        calculator calc = new Calculator();
+        Calculator calc = new Calculator();
         String exit = "no";
+        
         while(!exit.equals("yes")){
-            System.out.println("Enter first arg:");
-            String first = reader.next();
-            System.out.println("Enter second arg:");
-            String second = reader.next();
-            calc.add(Integer.valueOf(first), Integer.valueOf(second));
-            System.out.println("Result " + calc.getResult());
-            calc.cleanResult();
-            System.err.println("Exit : yes/no");
-            exit = reader.next();
-            
+
+       System.out.println("Enter type of operation(+ - * /):");
+       String answer = reader.next();
+       System.out.println("Enter first arg:");
+       String first = reader.next();
+       System.out.println("Enter second arg:");
+       String second = reader.next();
+        
+            switch (answer){
+                case "+": {
+                calc.add(Integer.valueOf(first), Integer.valueOf(second));
+                  }
+                case "-": {
+                calc.minus(Integer.valueOf(first), Integer.valueOf(second));
+                }
+               case "*": {
+                calc.multiplication(Integer.valueOf(first), Integer.valueOf(second));
+                }
+                case "/": {
+                calc.division(Integer.valueOf(first), Integer.valueOf(second));
+                }
+                
+                default: {
+                    System.out.println("Error operation");
+                }
+                System.out.println("Result " + calc.getResult());
+                calc.cleanResult();
+                System.err.println("Exit : yes/no");
+                exit = reader.next(); 
+
+            }
+          
         }
         
         }
